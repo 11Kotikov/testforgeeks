04b4f0ed45b05b8c3c4050b43e5c7d39e1daaf2f
Kotikov <11deadsluts@gmail.com>
2022-12-04 14:22:59 +0300

    created test array

--- testgeeks.cs
+++ testgeeks.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CreateTestArray ()
+{
+    System.Console.WriteLine("Input amount of the array's elements ");
+    int elementsAmount = Convert.ToInt32(Console.ReadLine());
+    string[] newArray = new string[elementsAmount];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+     System.Console.WriteLine($"Please input your symbols which will be in array on the {i+1} place");   
+        newArray[i] = Convert.ToString(Console.ReadLine());
+    }
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        System.Console.WriteLine(newArray[i]);
+    }
+}
+CreateTestArray ();
+
